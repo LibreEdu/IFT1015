4345bcbd46a1b8d45768d3707fcdad77aecfd29c
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2020-04-01 16:45:52 -0400

    Draw the tables

---
+++ TP2/documents/tp2a.js
@@ -0,0 +1,100 @@
+/* File: tp2a.js
+ * 
+ * Authors: Alexandre Pachot and Eliecer Rodriguez Silva
+ * Date: April 22, 2020
+ */
+
+
+// Return the html code of an array
+var htmlTable = function(inner) {
+  return '<table>\n\t<tbody>\n' + inner + '\t</tbody>\n</table>\n';
+}
+
+
+// Return the html code of a row
+var htmlTr = function(inner) {
+  return '\t\t<tr>\n' + inner + '\t\t</tr>\n';
+}
+
+
+// Return the html code of a cell
+var htmlTd = function(id, js, inner) {
+  var id = (id === '') ? '' : ' id="' + id + '"';
+  var js = (js == '') ? '' : ' ' + js;
+  return '\t\t\t<td' + id + js + '>' + inner + '</td>\n';
+}
+
+
+// Return the html code of an onclick cell
+var htmlTdOnclick = function(id, inner) {
+  return htmlTd(id, 'onclick="clic(' + id + ');"', inner);
+}
+
+
+// Return the html code of an image
+var htmlImg = function(img) {
+  return '<img src="cards/' + img + '.svg">'
+}
+
+
+// Array on the left
+var htmlDeck = function() {
+  
+  // New game button
+  var button = '<button onclick="init();" style="float: left;">';
+  button += 'Nouvelle partie</button';
+  
+  // Contents of the first line
+  var innerTR = htmlTd('', '', button);
+  innerTR += htmlTd('', '', '');
+  innerTR += htmlTdOnclick(25, htmlImg('back'));
+  innerTR += htmlTd('', '', '');
+  
+  // Contents of the array
+  var innerTable = htmlTr(innerTR);
+  
+  console.log(htmlTable(innerTable));
+  return htmlTable(innerTable);
+}
+
+
+// Array on the right
+var htmlGame = function() {
+  
+  var innerTable = '';
+  
+  // The first five lines
+  for(var i = 0; i < 5; i++) {
+    var innerTR = '';
+    
+    // Five columns of cards
+    for(var j = 0; j < 5; j++) {
+      innerTR += htmlTdOnclick(5*i + j, htmlImg('empty'));
+    }
+    
+    // Total column
+    innerTR += htmlTd('R'+i, '', '');
+    
+    innerTable += htmlTr(innerTR);
+  }
+  
+  var innerTR = '';
+  
+  //Totals line
+  for(var j = 0; j < 5; j++) {
+    innerTR += htmlTd('C'+j, '', '');
+  }
+  
+  // Big total
+  innerTR += htmlTd('T', '', 0);
+  
+  innerTable += htmlTr(innerTR);
+  
+  console.log(htmlTable(innerTable));
+  return htmlTable(innerTable);
+}
+
+var init = function () {
+  document.getElementById("b").innerHTML = htmlDeck() + htmlGame();
+  //document.getElementById("0").style.backgroundColor = "lime";
+  };
