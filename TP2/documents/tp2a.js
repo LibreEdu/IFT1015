bf8ac2839cea786052a6a043d4fe989f763c70af
Alexandre Pachot <alexandre.pachot@umontreal.ca>
2020-04-02 07:34:14 -0400

    game function, switch = OK

--- TP2/documents/tp2a.js
+++ TP2/documents/tp2a.js
@@ -7,13 +7,21 @@
 
 
 
+// Global variables. See also at the very bottom, at the end of the program.
+
+// In order to have a more easily configurable program
 var highlightColor = "lime";
-var transparent = "rgba(191, 255, 0, 0)"; // lime color + opacity = 0
-var focus; // Indicate which element has the focus
+var transparent = "rgba(191, 255, 0, 0)"; // any color (lime) + opacity = 0
 var nbColumns = 5;
 var nbLines = 5;
 var deckId = nbColumns * nbLines;
-var gameCards = Array(deckId + 1);
+
+// Indicate which element is highlighted
+var highlighted = '';
+
+// To know which card is where in order to calculate the points
+var gameCards = Array(deckId + 1); 
+
 
 
 
@@ -81,16 +89,16 @@ var htmlDeck = function() {
 
 
 
-// Table on the right (table 5x5)
+// Table on the right (table nbColumns x nbLines)
 var htmlGame = function() {
   
   var innerTable = '';
   
-  // The first five lines
+  // The first nbLines lines
   for(var i = 0; i < nbLines; i++) {
     var innerTR = '';
     
-    // Five columns of cards
+    // nbColumns columns of cards
     for(var j = 0; j < nbColumns; j++) {
       innerTR += htmlTdOnclick(nbColumns * i + j, htmlImg('empty'));
     }
@@ -182,85 +190,120 @@ var cardValue = function(cardNumber) {
 
 
 
-// Highlight a card
-var highlight = function(id) {
+// Highlight a card or remove the hight from a card
+var highlightSwitch = function(id) {
   var element = document.getElementById(id);
+  
   if ( element.style.backgroundColor == highlightColor ) {
     element.style.backgroundColor = transparent;
-    focus = '';
+    highlighted = '';
   } else {
     element.style.backgroundColor = highlightColor;
-    focus = id;
+    highlighted = id;
   }
+  
 };
 
 
 
 
-// Click on the deck evenement
+// We are on the deck, the left table
 var deck = function() {
   var element = document.getElementById(deckId);
+  
+  // If the deck image is the backside image of a card
   if (element.innerHTML == htmlImg('back')) { 
-    // We turn over a new card
+
+    // We flip the card to get a new card
     var newCard = cards.pop();
     element.innerHTML = htmlImg(cardValue(newCard));
     gameCards[deckId] = newCard;
-    //console.log(gameCards);
   }
   
-  highlight(deckId);
+  // If a card from the right is highlighted, the highlight is removed
+  if (highlighted !== "" && highlighted < deckId) {
+    highlightSwitch(highlighted);
+  }
+  
+  // We switch the highlight of the deck
+  highlightSwitch(deckId);
 };
 
 
 
 
-// Click on the deck evenement
+// We are on the game, the right table
 var game = function(id) {
   var element = document.getElementById(id);
   
+  // If there's no card
   if (element.innerHTML == htmlImg('empty')) {
-    // Click on an empty cell
 
-    if (focus === "") {
-      // No cell has the focus, there's nothing to do.
+    // And that no card is highlighted
+    if (highlighted === "") {
+      
+      // There's nothing to be done
       return;
     }
     
-    // One cell has the focus, we get his image 
-    element.innerHTML = document.getElementById(focus).innerHTML;
+    // The image of the card is replaced by the image of the card that is
+    // highlighted
+    element.innerHTML = document.getElementById(highlighted).innerHTML;
     
-    // Operation to do, depending on the origin of the focus
-    if (focus == deckId) {
+    // We just moved a card into an empty slot. The card that was highlighted,
+    // if it's from the deck, the card underneath is the back of a card.
+    // Otherwise, it's an empty slot in the game.
+    if (highlighted == deckId) {
       var image = 'back';
       //gameCards[id]= gameCards[deckId];
     } else {
       var image = 'empty';
       //gameCards[id]= gameCards[deckId];
     }
-    //console.log(gameCards);
     
-    // Change the focus image
-    document.getElementById(focus).innerHTML = htmlImg(image);
-    highlight(focus);
+    // We're changing the image of the card that was highlighted
+    document.getElementById(highlighted).innerHTML = htmlImg(image);
     
-  } else {
-    // We click on a cell that is not empty
+    // And we remove the highlight
+    highlightSwitch(highlighted);
     
+  } else { // There's a card
+    
+    // Did we clicked on a highlighted card?
     if (element.style.backgroundColor == highlightColor) {
-      // The cell's hightlighted, we remove the hightlight
-      highlight(id);
-    } else {
-      // The cell's not hightlighted
       
-      if (focus === "") {
-        // No cell has focus, we hightlight the cell
-        highlight(id);
-      } else {
-        // Switch 
-        var temp = element.innerHTML;
-        element.innerHTML = document.getElementById(focus).innerHTML;
-        document.getElementById(focus).innerHTML = temp;
-        highlight(focus);
+      // If that's the case, we remove the highlight from the slot
+      highlightSwitch(id);
+      
+    } else {
+      // The slot isn't highlighted
+      
+      // Is there a slot, elsewhere, that's highlighted?
+      if (highlighted === "") {
+        
+        // No slot is highlighted, we hightlight the slot
+        highlightSwitch(id);
+        
+      } else { // We just clicked on a slot that isn't highlighted and another
+        // slot is highlighted. We're switching the two cards.
+        
+        // If the hightlight was on the deck, we don't switch
+        if (highlighted === deckId) {
+          
+          // We remove the highlight from the deck
+          highlightSwitch(deckId);
+          
+          // We're highlighting where we are
+          highlightSwitch(id);
+          
+        } else { // Switching the two cards
+            var temp = element.innerHTML;
+            element.innerHTML = document.getElementById(highlighted).innerHTML;
+            document.getElementById(highlighted).innerHTML = temp;
+            
+            // We remove the highlight from the old slot
+            highlightSwitch(highlighted);
+        }
       }
     }
   }
@@ -271,7 +314,7 @@ var game = function(id) {
 
 // Clic dispatcher
 var clic = function(id) {
-  if ( id == deckId) {
+  if ( id === deckId) {
     deck();
   } else {
     game(id);
